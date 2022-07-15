92c81e34331bb236285be645d7fef7125fa6514d
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-07-15 14:28:33 +0700

    Add files via upload

---
+++ lib/nekonime.rb
@@ -0,0 +1,25 @@
+require_relative 'request'
+require_relative 'webUrl'
+
+class Neko
+  def initialize
+    @neko = ''
+  end
+
+  def display_neko
+    get_neko()
+    neko
+  end
+
+  private
+
+  attr_accessor :neko
+
+  def get_neko
+    web = "api/neko"
+    res = Request.new_request(Url.new.nekos(web))
+    @neko = res['neko']
+  end
+end
+
+Neko.new.display_neko
---
+++ lib/webUrl.rb
@@ -0,0 +1,9 @@
+class Url
+  def initialize
+    @url = ''
+  end
+
+  def nekos(v)
+    @url = "https://nekos.life/#{v}"
+  end
+end

9e66d00a7c468fa07e7ea7e907c3b7c2ea93723a
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-07-16 16:21:36 +0700

    Update bot.rb
    Signed-off-by: ナスワサルサ <75057738+moo-d@users.noreply.github.com>

--- bot.rb
+++ bot.rb
@@ -3,6 +3,8 @@ require_relative('lib/command/nekonime')
 
 token = 'YOUR_TOKEN_HERE'
 
+$ytmp3_request = nil
+
 def command(bot, message)
   id = message.chat.id
 
@@ -25,6 +27,44 @@ def command(bot, message)
       chat_id: id,
       photo: neko
     )
+  end
+  when '/ytmp3'
+    $ytmp3_request = true
+    client.send_message(
+      chat_id: id,
+      text: "Send the youtube url!"
+    )
+  else
+    if $ytmp3_request
+      url = message.text
+      if url.include? "http"
+        data = Ytmp3.new.display_ytmp3(url)
+        title = data['title']
+        thumb = data['thumb']
+        size = data['size']
+        audioUrl = data['download_audio']
+        client.send_photo(
+          chat_id: id,
+          photo: thumb,
+          caption: "- Title: #{title}\n- Size: #{size}\n\nWait a moment"
+        )
+        client.send_audio(
+          chat_id: id,
+          audio: audioUrl,
+          caption: "This is your mp3."
+        )
+      else
+        client.send_message(
+          chat_id: id,
+          text: "Your url is invalid!"
+        )
+      end
+    else
+      client.send_message(
+        chat_id: id,
+        text: 'I do not recognize that command'
+      )
+    end
   end
 end
 

853eac9082306be131052dbd464e2255c39cd5e3
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-07-17 22:37:23 +0700

    Add files via upload

---
+++ lib/menu.rb
@@ -0,0 +1,17 @@
+module Menu
+  def self.new_menu(name)
+    "Hello, #{name}
+
+
+Main Commands:
+- /start - Show menu
+- /stop - Say bye to user
+
+Downloader Commands:
+- /ytmp3 - Download youtube audio
+- /ytmp4 - Download youtube video
+
+Anime Commands:
+- /nekonime - Send neko anime image"
+  end
+end
